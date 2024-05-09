293d80d1941513224a16cd8df7ce10b24051bb1a
PrateekTechmatys <prateeky562@gmail.com>
2024-05-09 23:41:15 +0530

    making the design

--- src/components/Password.jsx
+++ src/components/Password.jsx
@@ -1,30 +1,45 @@
 import React, { useState } from 'react'
 
 const Password = () => {
+    const [password, setPassword] = useState("");
     const [upperCase, setUpperCase] = useState(false);
     const [lowerCase, setLowerCase] = useState(false);
     const [number, setNumber] = useState(false);
     const [symbol, setSymbol] = useState(false);
     const [length, setLength] = useState(false);
+    const generatePassword =(a,b,c,d)=>{
+        let password = "";
+        let characters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890!@#$%^&*()";
+        for (let i = 0; i < length; i++) {
+                if(a && b && c && d){
+                    
+                    password += characters.charAt(Math.floor(Math.random() * characters.length));
+                }
+        }
+        return password;
+    }
     const handleSubmit = (e) => {
         e.preventDefault()
         console.log(upperCase, lowerCase, symbol, number);
         if(length >=8 && length<=50){
             console.log("Password Length is valid");
             if (upperCase && lowerCase && number && symbol) {
-                console.log("Password is valid");
+                console.log("All conditions are valid");
+               let ans = generatePassword(upperCase, lowerCase, number, symbol, length);
+               setPassword(ans);
             }
         }
         else{
             console.log("Password Length is not valid");
             alert("Password Length is not valid");
+            return;
         }
 
 
     }
     return (
         <div>
-            <input type="text" placeholder="Password" />
+            <input type="text" placeholder="Password"  value={password}/>
             <form onSubmit={handleSubmit} >
 
                 <div className='flex '>
